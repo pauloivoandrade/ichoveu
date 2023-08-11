fd34545ab5c5cab1344b891c9428efb466cc6074
pauloivoandrade <pauloivoasantos@gmail.com>
2023-08-11 00:50:45 -0300

    Quesito 01 OK

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -9,11 +9,15 @@ export const searchCities = async (term) => {
       window.alert('Nenhuma cidade encontrada');
     } return data;
   } catch (error) {
-    console.error('Error:', error);
+    console.error(error);
     return [];
   }
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
-};
+// export const getWeatherByCity = async (cityURL) => {
+//   const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+//   try{
+//     const response = await fetch(URL_API);
+//     const data = await response.json();
+//   }
+// };
