4edda3e37a05cd5f4934c2bbe8dc1a3e41ef8851
pauloivoandrade <pauloivoasantos@gmail.com>
2023-08-11 10:07:41 -0300

    Feat: Function getWeatherByCiry

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -14,10 +14,14 @@ export const searchCities = async (term) => {
   }
 };
 
-// export const getWeatherByCity = async (cityURL) => {
-//   const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
-//   try{
-//     const response = await fetch(URL_API);
-//     const data = await response.json();
-//   }
-// };
+export const getWeatherByCity = async (cityURL) => {
+  const URL_API = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`;
+  const response = await fetch(URL_API);
+  const data = await response.json();
+  return {
+    name: data.location.name,
+    temp: data.location.temp_c,
+    condition: data.location.condition,
+    icon: data.location.icon,
+  };
+};
