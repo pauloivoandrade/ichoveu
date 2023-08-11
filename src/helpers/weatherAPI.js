e54751d91497f0821811e1b2aeeb79a677424cc4
pauloivoandrade <pauloivoasantos@gmail.com>
2023-08-11 00:00:10 -0300

    Feat: Function searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,8 +1,12 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+  const response = await fetch(URL_API);
+  const data = await response.json();
+  return data;
 };
+console.log(searchCities('sao paulo'));
 
 export const getWeatherByCity = (/* cityURL */) => {
 //   seu código aqui
