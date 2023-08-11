768c9b83a94e79eb280231ae45ab91a5aa9040e4
pauloivoandrade <pauloivoasantos@gmail.com>
2023-08-11 00:09:18 -0300

    Fix: TOKEN

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,12 +1,18 @@
-const TOKEN = import.meta.env.VITE_TOKEN;
+const TOKEN = b4cdc8159a994687a6422044231108;
 
 export const searchCities = async (term) => {
-  const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
-  const response = await fetch(URL_API);
-  const data = await response.json();
-  return data;
+  try {
+    const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+    const response = await fetch(URL_API);
+    const data = await response.json();
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+    } return data;
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
 };
-console.log(searchCities('sao paulo'));
 
 export const getWeatherByCity = (/* cityURL */) => {
 //   seu código aqui
